81ff7e65b0b00b0688514176889966f436c4d3ee
cascharly <cascharly@gmail.com>
2021-04-15 19:30:24 -0300

    092 rutas

--- server/server.js
+++ server/server.js
@@ -1,11 +1,10 @@
-require ('./config/config')
+require("./config/config");
 
 const express = require("express");
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const app = express();
 const bodyParser = require("body-parser");
-
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -13,40 +12,18 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-app.get("/usuario", function (req, res) {
-  res.json("get Usuario");
-});
+app.use(require("./routes/usuario"));
 
-app.post("/usuario", function (req, res) {
-  let body = req.body;
-
-  if (body.nombre === undefined) {
-    res.status(400).json({
-      ok: false,
-      mensaje: " El nombre es necesario",
-    });
-  } else {
-    res.json({ persona: body });
+const uri =
+  "mongodb+srv://mern_user:NrEvsdGFJAa0Rs8U@cluster0.fx5ag.mongodb.net/cafe";
+mongoose.connect(
+  uri,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err, res) => {
+    if (err) throw err;
+    console.log("Base de datos online");
   }
-});
-
-app.put("/usuario/:id", function (req, res) {
-  let id = req.params.id;
-  res.json({
-    id,
-  });
-});
-
-app.delete("/usuario", function (req, res) {
-  res.json("delete Usuario");
-});
-
-const uri = 'mongodb+srv://mern_user:NrEvsdGFJAa0Rs8U@cluster0.fx5ag.mongodb.net/cafe';
-mongoose.connect(uri,{useNewUrlParser: true, useUnifiedTopology: true}, (err,res) => {
-  if (err) throw err;
-  console.log('Base de datos online');
-});
-
+);
 
 app.listen(process.env.PORT, () => {
   console.log("Escuchando en puerto:", process.env.PORT);
