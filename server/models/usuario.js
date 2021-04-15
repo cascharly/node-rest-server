63a9ab9dc4fa450098d6eba7d67e4faa43c95bae
cascharly <cascharly@gmail.com>
2021-04-15 19:56:20 -0300

    Modelo de usuarios

---
+++ server/models/usuario.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+let Schema = mongoose.Schema;
+
+let usuarioSchema = new Schema({
+  nombre: {
+    type: String,
+    required: [true, "El nombre es requerido"],
+  },
+  email: {
+    type: String,
+    required: [true, "El correo es requerido "],
+  },
+  password: {
+    type: String,
+    required: [true, "La contraseña es requerida"],
+  },
+  img: {
+    type: String,
+    required: false,
+  }, // no es obligatoria
+  role: {
+    default: "USER_ROLE",
+  }, // default: 'USER_ROLE'
+  estado: {
+    type: Boolean,
+    default: true,
+  }, // Boolean
+  google: {
+    type: Boolean,
+    default: false,
+  }, // Boolean
+});
+
+module.exports = mongoose.model("Usuario", usuarioSchema);
