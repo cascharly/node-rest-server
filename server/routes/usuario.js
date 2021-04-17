929097bc2b38b81d5a412db01878668281283258
cascharly <cascharly@gmail.com>
2021-04-17 10:20:25 -0300

    DELETE user

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -12,7 +12,7 @@ app.get("/usuario", function (req, res) {
   let limite = req.query.limite || 5;
   limite = Number(limite);
 
-  Usuario.find({},'nombre email role estado google img')
+  Usuario.find({}, "nombre email role estado google img")
     .skip(desde)
     .limit(limite)
     .exec((err, usuarios) => {
@@ -85,8 +85,30 @@ app.put("/usuario/:id", function (req, res) {
 });
 
 // DELETE  --------------------------------------------------------------------------
-app.delete("/usuario", function (req, res) {
-  res.json("delete Usuario");
+app.delete("/usuario/:id", function (req, res) {
+  let id = req.params.id;
+  Usuario.findByIdAndRemove(id, (err, usuarioBorrado) => {
+    if (err) {
+      return res.status(400).json({
+        ok: false,
+        err,
+      });
+    }
+
+    if ( !usuarioBorrado ) {
+      return res.status(400).json({
+        ok:false,
+        err: {
+          message: 'Usuario no encontrado'
+        }
+      })
+    }
+
+    res.json({
+      ok: true,
+      usuario: usuarioBorrado,
+    });
+  });
 });
 
 module.exports = app;
